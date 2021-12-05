49e3d0da3883858e12382d8d6a7dc4259cb314e8
Ashish Aryal <ashish@kortaben.se>
2021-12-05 13:15:18 +0100

    node mail stuff

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,6 +69,9 @@ export default {
     '@nuxtjs/google-fonts',
     '@braid/vue-formulate/nuxt',
     ['nuxt-mail', {
+      message: [
+        { name: 'contact', to: 'ashisharyal64@gmail.com' }
+      ],
       smtp: {
         host: 'smtp.postmarkapp.com',
         port: 2525,
