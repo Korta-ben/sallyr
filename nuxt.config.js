49ef177c76e0a3325939444df04570ef0b55639d
Ashish Aryal <ashish@kortaben.se>
2022-03-02 16:57:18 +0100

    gtags

--- nuxt.config.js
+++ nuxt.config.js
@@ -154,6 +154,7 @@ export default {
   },
   googleAnalytics: {
     id: 'UA-75786022-1', //Your ID here
+    debug: { sendHitTask: true },
     beforeFirstHit (){
       if ($nuxt.$cookieUniversal.get('cookie_control_consent') && $nuxt.$cookieUniversal.get('cookie_control_enabled_cookies') === '_gat_gtag_UA-75786022-1'){
         $nuxt.$ga.enable();
