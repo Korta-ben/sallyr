78d6b4d9b25fd1691e0f72e8f5ca6a677dbb18c8
Ashish Aryal <ashish@kortaben.se>
2022-03-02 17:36:09 +0100

    graphql removed

--- nuxt.config.js
+++ nuxt.config.js
@@ -127,42 +127,42 @@ export default {
         src:  'https://www.googletagmanager.com/gtag/js?id=UA-75786022-1',
         async:  true,
         cookies:["ga", "gat_gtag_UA-75786022-1", "gid"],
-        // accepted: () =>{
-        //   // window.dataLayer = window.dataLayer || [];
-        //   // window.dataLayer.push({
-        //   //   'gtm.start': new Date().getTime(),
-        //   //   event: 'gtm.js',
-        //   //   'config': 'UA-75786022-1'
-        //   // });
-        //   // window.dataLayer = window.dataLayer || [];
-        //   // function gtag() {
-        //   //   dataLayer.push(arguments);
-        //   // }
-        //   // gtag("js", new Date());
-        //   // gtag("config", "UA-75786022-1");
-        //   // window.dataLayer = window.dataLayer || [];
-        //   // function gtag(){dataLayer.push(arguments);}
-        //   // gtag('js', new Date());
-        //   // gtag('config', 'UA-75786022-1');
-        // },
-        // declined: () =>{
-        // }
+        accepted: () =>{
+          window.dataLayer = window.dataLayer || [];
+          window.dataLayer.push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js',
+            'config': 'UA-75786022-1'
+          });
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag("js", new Date());
+          gtag("config", "UA-75786022-1");
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-75786022-1');
+        },
+        declined: () =>{
+        }
       }
 
     ]
 
   },
-  googleAnalytics: {
-    id: 'UA-75786022-1', //Your ID here
-    debug: { sendHitTask: true },
-    beforeFirstHit (){
-      if ($nuxt.$cookieUniversal.get('cookie_control_consent') && $nuxt.$cookieUniversal.get('cookie_control_enabled_cookies') === 'ga'){
-        $nuxt.$ga.enable();
-      } else {
-        $nuxt.$ga.disable();
-      }
-    }
-  },
+  // googleAnalytics: {
+  //   id: 'UA-75786022-1', //Your ID here
+  //   debug: { sendHitTask: true },
+  //   beforeFirstHit (){
+  //     if ($nuxt.$cookieUniversal.get('cookie_control_consent') && $nuxt.$cookieUniversal.get('cookie_control_enabled_cookies') === 'ga'){
+  //       $nuxt.$ga.enable();
+  //     } else {
+  //       $nuxt.$ga.disable();
+  //     }
+  //   }
+  // },
 
   formulate:{
     options:{
