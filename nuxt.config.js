068b26b7c93c2b8ac8892aada29aec966701ca03
Ashish Aryal <ashish@kortaben.se>
2022-03-02 17:24:45 +0100

    graphql removed

--- nuxt.config.js
+++ nuxt.config.js
@@ -127,26 +127,26 @@ export default {
         src:  'https://www.googletagmanager.com/gtag/js?id=UA-75786022-1',
         async:  true,
         cookies:["_ga", "_gat_gtag_UA-75786022-1", "_gid"],
-        accepted: () =>{
-          // window.dataLayer = window.dataLayer || [];
-          // window.dataLayer.push({
-          //   'gtm.start': new Date().getTime(),
-          //   event: 'gtm.js',
-          //   'config': 'UA-75786022-1'
-          // });
-          // window.dataLayer = window.dataLayer || [];
-          // function gtag() {
-          //   dataLayer.push(arguments);
-          // }
-          // gtag("js", new Date());
-          // gtag("config", "UA-75786022-1");
-          // window.dataLayer = window.dataLayer || [];
-          // function gtag(){dataLayer.push(arguments);}
-          // gtag('js', new Date());
-          // gtag('config', 'UA-75786022-1');
-        },
-        declined: () =>{
-        }
+        // accepted: () =>{
+        //   // window.dataLayer = window.dataLayer || [];
+        //   // window.dataLayer.push({
+        //   //   'gtm.start': new Date().getTime(),
+        //   //   event: 'gtm.js',
+        //   //   'config': 'UA-75786022-1'
+        //   // });
+        //   // window.dataLayer = window.dataLayer || [];
+        //   // function gtag() {
+        //   //   dataLayer.push(arguments);
+        //   // }
+        //   // gtag("js", new Date());
+        //   // gtag("config", "UA-75786022-1");
+        //   // window.dataLayer = window.dataLayer || [];
+        //   // function gtag(){dataLayer.push(arguments);}
+        //   // gtag('js', new Date());
+        //   // gtag('config', 'UA-75786022-1');
+        // },
+        // declined: () =>{
+        // }
       }
 
     ]
@@ -157,13 +157,7 @@ export default {
     debug: { sendHitTask: true },
     beforeFirstHit (){
       if ($nuxt.$cookieUniversal.get('cookie_control_consent') && $nuxt.$cookieUniversal.get('cookie_control_enabled_cookies') === 'ga'){
-        // $nuxt.$ga.enable();
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-          dataLayer.push(arguments);
-        }
-        gtag("js", new Date());
-        gtag("config", "UA-75786022-1");
+        $nuxt.$ga.enable();
       } else {
         $nuxt.$ga.disable();
       }
