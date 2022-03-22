42b2fc49b4745cbf7836ec4923213811ccbc87c3
64ashish <ashish@kortaben.se>
2022-03-22 09:52:59 +0100

    article quote image safari fix

--- components/articles/ArticleQuotes.vue
+++ components/articles/ArticleQuotes.vue
@@ -3,7 +3,7 @@
     <div class="px-5  max-w-screen-1.5lg m-auto">
       <div v-for="quote in block.quotes" class="flex flex-col
       lg:grid lg:grid-cols-4 lg:justify-items-center lg:items-center">
-        <img :src="quote.image" class="m-auto pb-7.5 px-21.75">
+        <img :src="quote.image" class="object-contain m-auto pb-7.5 px-21.75">
         <p v-html="quote.quote" class="pb-6 text-2xl font-medium lg:col-span-3"></p>
         <p v-html="quote.reference" class="font-normal text-lg lg:col-span-4"></p>
       </div>
