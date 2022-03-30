6d48e7f0d35e4746792d41ebf29b55e03182068d
Ashish <ashish@kortaben.se>
2022-03-30 08:20:32 +0200

    newsletter form added

--- components/articles/ArticleNewsletter.vue
+++ components/articles/ArticleNewsletter.vue
@@ -5,10 +5,12 @@
       class="text-srwhite font-bold text-4xl lg:text-12.5 text-center leading-10 pb-5 lg:leading-11"></h2>
       <p v-html="block.newsletter_subtext"
          class="text-srwhite font-bold text-xl lg:text-2xl text-center"></p>
-      <!--    <script charset="utf-8" type="text/javascript" src="//js-eu1.hsforms.net/forms/v2.js"></script>-->
       <div class="max-w-4xl m-auto">
+        <script charset="utf-8" type="text/javascript" src="//js-eu1.hsforms.net/forms/v2.js"></script>
         <script>
-          hbspt.forms.create({
+
+
+        hbspt.forms.create({
             region: "eu1",
             portalId: "25028945",
             formId: "ba72938e-1259-4322-ace9-3db028331e78"
